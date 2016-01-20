ce50be01087cf8281abb7d42dd7971c8cbd75d63
by alvin <by alvin@User>
2016-01-20 18:58:22 -0500

    segundo commit branch

---
+++ is2btest/src/main/java/com/mycompany/is2btest/Error.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.is2btest;
+
+/**
+ *
+ * @author by alvin
+ */
+public class Error {
+    fgnjhkf strst rddty
+}
